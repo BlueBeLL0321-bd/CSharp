d67d2b58b9e19fffe88df51a2d868e7678a84a2c
DESKTOP-5V3FRMH\M <leeyongwoo736@gmail.com>
2025-03-18 16:23:14 +0900

    1. Private & Public 예제 2 2. Constructor & Destructor 예제 3. Method 예제

--- Assets/Scripts/PrivatePublic/PublicAndPrivate.cs
+++ Assets/Scripts/PrivatePublic/PublicAndPrivate.cs
@@ -23,6 +23,12 @@ namespace PrivatePublic
 
             // [5] Car 클래스의 객체를 통해 private한 메서드에 접근해서 사용 (X)
             // myCar.Bye();
+
+            // [6] private한 필드(age)를 외부에 공개하고자 할 때 public한 메서드를 이용한다
+            // myCar.age = 21;
+            myCar.SetAge(21);
+            int myAge = myCar.GetAge();
+            Debug.Log(myAge);
         }
     }
 }
